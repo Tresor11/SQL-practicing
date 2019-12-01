574b16d4261a4bd2ef20861290e06ddab121a633
Tresor11 <tresor.moise2001@gmail.com>
2019-12-01 09:07:35 +0200

    select name and population whose name end with a certain letter

--- file.sql
+++ file.sql
@@ -1,3 +1,15 @@
-SELECT name, population
-  FROM world
- WHERE population BETWEEN 1000000 AND 1250000
+SELECT
+  name,
+  population
+FROM
+  world
+WHERE
+  population BETWEEN 1000000
+  AND 1250000
+SELECT
+  name,
+  population
+FROM
+  world
+WHERE
+  name LIKE "Al%"
